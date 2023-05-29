7a89a9aa371e0b03486b3b9d4d7f819e8ee0ec3c
lyh11111111 <133839927+lyh11111111@users.noreply.github.com>
2023-05-29 21:55:05 +0900

    111

---
+++ 책숲4차심화/책숲4차심화/책숲4차심화/01.js
@@ -0,0 +1,13 @@
+/*const element=document.getElementsByClassName("name");
+
+const btn=document.getElementsByClassName("show");
+
+function show(){
+     if(element.style.display!='none'){
+        element.style.display = 'none';
+     }
+     else()=>
+        element.style.display='block';
+     }
+    
+*/
